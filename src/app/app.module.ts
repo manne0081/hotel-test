ae1be0cf4880a7e9969d7e8703a4ea61a751c02a
ManuelSchmidt <manne0081@gmail.com>
2022-02-09 16:12:35 +0100

    reactiveValidation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -22,6 +22,7 @@ import { EventSignupComponent } from './event-signup/event-signup.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService),
   ],
--- src/app/bookings/add-booking/add-booking.component.ts
+++ src/app/bookings/add-booking/add-booking.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Booking } from 'src/app/model/booking';
 import { BookingService } from '../booking.service';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-add-bookings',
@@ -18,20 +19,43 @@ export class AddBookingComponent implements OnInit {
         endDate: new Date()
     }
 
+    bookingForm = this.formBuilder.group({
+        id:['', Validators.required],
+        name:['', Validators.compose([Validators.required, Validators.minLength(5)])],
+        roomNumber:['', Validators.required],
+        startDate:['', Validators.required],
+        endDate:['', Validators.required],
+    })
+
     constructor(private router: Router,
                 private activatedRoute: ActivatedRoute,
-                private bookingService: BookingService) { }    
+                private bookingService: BookingService,
+                private formBuilder: FormBuilder) { }    
 
     ngOnInit(): void {
         if (this.router.url != "/addBooking") {
             var id = Number(this.activatedRoute.snapshot.paramMap.get("id"));
-            this.bookingService.getBookingById(id).subscribe((result) => {
+
+            this.bookingService.getBookingById(id).subscribe((result) => {                
                 this.booking = result;
+                this.bookingForm.setValue({
+                    id: this.booking.id,
+                    roomNumber: this.booking.roomNumber,
+                    name: this.booking.name,
+                    startDate: this.booking.startDate,
+                    endDate: this.booking.endDate,
+                });
             });
         }        
     }
     
     save(): void {
+        this.booking.id = this.bookingForm.get('id')?.value;
+        this.booking.name = this.bookingForm.get('name')?.value;
+        this.booking.roomNumber = this.bookingForm.get('roomNumber')?.value;
+        this.booking.startDate = this.bookingForm.get('startDate')?.value;
+        this.booking.endDate = this.bookingForm.get('endDate')?.value;
+
         this.bookingService.addBooking(this.booking).subscribe();
         this.router.navigate(['bookings']);
     }
